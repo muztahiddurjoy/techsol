91d5b3d7181813f8feb86163d2fe84c6c7889f07
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-05 04:09:04 +0600

    solar header

--- app/solar/page.tsx
+++ app/solar/page.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/Footer/Footer'
 import Navbar from '@/components/Navbar/Navbar'
+import Header from '@/components/Solar/Header/Header'
 import React from 'react'
 
 const Solar = () => {
   return (
     <>
     <Navbar/>
+    <Header/>
     <Footer/>
     </>
   )
