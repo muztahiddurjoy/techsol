5d9bad3f04d8d30aee3c36e94318fefe473192c0
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-06 01:03:14 +0600

    product description

--- components/SingleProduct/ProductContainer/ProductContainer.tsx
+++ components/SingleProduct/ProductContainer/ProductContainer.tsx
@@ -1,9 +1,39 @@
+import { generatorsets } from '@/components/Navbar/genetatorSets'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const ProductContainer = () => {
+const ProductContainer = ({link}:{link:string}) => {
   return (
-    <div>ProductContainer</div>
+    <div className='grid grid-cols-1 md:grid-cols-5 md:px-20 px-5 mt-10'>
+      <div className="hidden md:block">
+            <p className='font-bold text-gray-600 uppercase'>PRODUCTS</p>
+            <div className="w-16 my-3 h-[3px] bg-gray-300"></div>
+            <ul>
+                {generatorsets.map((v,i)=> <li className={`my-3 text-sm ${v.link.includes(link)&&'font-bold text-black'} hover:text-black text-primary`}><Link href={v.link}>{v.title}</Link></li>)}
+            </ul>
+        </div>
+        <div className="col-span-4 grid grid-cols-1 md:grid-cols-2">
+          <div className="flex items-center justify-center">
+          <Image src="/gen.jpg" height={500} width={600} alt=''/>
+          </div>
+          <div className="flex justify-end items-center pl-10">
+            <div className="rounded-lg bg-gray-200 p-5 flex items-end justify-center flex-col h-full w-full">
+              <Link className='text-sm text-gray-500 uppercase text-right' href="/">Doosan</Link>
+              <p className='text-3xl text-gray-800 font-bold text-right'>EN170D</p>
+              <div className="h-[2px] w-10 bg-gray-400 my-5"></div>
+              <p className='font-bold text-lg text-gray-700 text-right'>Datasheet</p>
+              <Link href="/"><Button className='rounded-none mt-5' size="lg">EN170D</Button></Link>
+              <div className="flex items-center justify-end text-xs mt-4 ">
+                <p className='text-gray-500'>Category: </p>
+                <Link href="/" className='text-primary ml-2'> Doosan</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
-export default ProductContainer
+export default ProductContainer 
--- components/SingleProduct/ProductDesc/ProductDesc.tsx
+++ components/SingleProduct/ProductDesc/ProductDesc.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 const ProductDesc = () => {
   return (
-    <div>ProductDesc</div>
+    <div className="md:px-14 px-5">
+      <Tabs defaultValue="genset" className='bg-gray-300 p-3 rounded-xl'>
+        <TabsList className='bg-transparent'>
+          <TabsTrigger className='font-semibold uppercase text-gray-500 data-[state=active]:bg-primary data-[state=active]:text-white rounded-full text-xs px-2' value="genset">Genset</TabsTrigger>
+          <TabsTrigger className='font-semibold uppercase text-gray-500 data-[state=active]:bg-primary data-[state=active]:text-white rounded-full text-xs px-2' value="engine">Engine</TabsTrigger>
+          <TabsTrigger className='font-semibold uppercase text-gray-500 data-[state=active]:bg-primary data-[state=active]:text-white rounded-full text-xs px-2' value="alternator">Alternator</TabsTrigger>
+          <TabsTrigger className='font-semibold uppercase text-gray-500 data-[state=active]:bg-primary data-[state=active]:text-white rounded-full text-xs px-2' value="dimensions">Dimensions</TabsTrigger>
+        </TabsList>
+        <TabsContent value="genset">
+          <div className="grid grid-cols-3 text-sm text-gray-600 [&>*]:my-2 md:px-4">
+            <p>Kire</p>
+            <div>Kemon asos</div>
+            <p>Kire</p>
+            <p>Kire</p>
+            <div>Kemon asos</div>
+            <p>Kire</p>
+          </div>
+        </TabsContent>
+        <TabsContent value="engine"> 
+          <div className="grid grid-cols-3 text-sm text-gray-600 [&>*]:my-2 md:px-4">
+            
+          </div>
+        </TabsContent>
+        <TabsContent value="alternator">
+           <div className="grid grid-cols-3 text-sm text-gray-600 [&>*]:my-2 md:px-4">
+            
+            </div>
+        </TabsContent>
+        <TabsContent value="dimensions">
+        <div className="grid grid-cols-3 text-sm text-gray-600 [&>*]:my-2 md:px-4">
+            
+            </div>
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
