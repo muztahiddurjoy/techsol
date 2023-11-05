4b07e4b81dabd7c13d1c10aba7193dacc364dfc7
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-06 01:59:34 +0600

    mobile nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -94,6 +94,65 @@ const Navbar = () => {
         <Button size="sm" className='ml-3 rounded-full border-2 border-primary hover:bg-primary/60'>Request a Quote</Button>
         </div>
     </div>
+    <div className={`fixed bg-white transition-all ease-in-out duration-300 ${scroll>300?'top-0 opacity-100':'-top-20 opacity-0'} right-0 left-0 grid md:flex grid-cols-7 items-center md:px-20 md:py-5 px-3 py-3 z-40`}>
+      <div className="block md:hidden" onClick={e=> setopen(true)}>
+        <Menu className='stroke-black'/>
+      </div>
+      <div className="col-span-3 md:col-span-1 flex items-center justify-center md:justify-start">
+        <Link href="/" className='pl-4 md:pl-0'><Image alt='Maskan' height={70} width={150} src="/logo.png"/></Link>
+      </div>
+        <ul className='md:flex items-center hidden text-xs uppercase font-medium text-gray-600 [&>*]:px-3 [&>*]:border-r [&>*]:border-gray-600 px-4 md:px-10'>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/company-profile">Company Profile</Link>
+            </li>
+            <li>
+            <HoverCard closeDelay={100} openDelay={0}>
+            <HoverCardTrigger className='flex items-center'>Generator Sets <ChevronDown height={10}/> </HoverCardTrigger>
+            <HoverCardContent className='mt-3 bg-primary ring-theme/90 text-white'>
+                <ul>
+                {generatorsets.map((v,i)=> <li className='my-2' key={i}>
+                    <Link href={v.link}>{v.title}</Link>
+                    {i!==generatorsets.length-1&&<Separator orientation='horizontal' className=' bg-gray-400 my-2'/>}
+                </li>)}
+                </ul>
+            </HoverCardContent>
+            </HoverCard>
+            </li>
+            <li>
+                <Link href="/synchronization-system">SYNCHRON SYSTEM</Link>
+            </li>
+            <li>
+                <Link href="/special-project">Special Project</Link>
+            </li>
+            <li>
+                <Link href="/solar">Solar</Link>
+            </li>
+            <li  >
+                <Link href="/contact">Contact</Link>
+            </li>
+        </ul>
+        <div className="md:flex hidden items-center">
+        <HoverCard closeDelay={100} openDelay={0}>
+            <HoverCardTrigger><Search className='stroke-gray-600' height={15}/></HoverCardTrigger>
+            <HoverCardContent className='mt-3 bg-primary ring-theme/90 text-white'>
+                <div className="grid grid-cols-10">
+                  <div className="col-span-8">
+                    <Input value={search} onChange={e=> setsearch(e.target.value)} placeholder='Search...' className='bg-white border-white text-black'/>
+                  </div>
+                  <div className="col-span-2 bg-primary">
+                    <Button className='bg-red-800 hover:bg-red-900'><Search height={15} className='stroke-white'/></Button>
+                  </div>
+                </div>
+            </HoverCardContent>
+            </HoverCard>
+        </div>
+        <div className="col-span-3 md:col-span-1 flex justify-end">
+        <Button size="sm" className='ml-3 rounded-full border-2 border-primary hover:bg-primary/60'>Request a Quote</Button>
+        </div>
+    </div>
     <div onClick={()=> window.scrollTo({top:0,behavior:'smooth'})} className={`rounded-full ${scroll<500?'opacity-0 mt-10':'opacity-100'} z-50 transition-all ease-in-out duration-300 hover:bg-white cursor-pointer fixed bottom-5 border border-red-400 h-10 w-10 flex items-center justify-center right-5`}>
       <ChevronUp height={20} className='stroke-red-400'/>
     </div>
