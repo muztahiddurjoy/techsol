860263e46f70580182f38a964b082dc6f95c5bb1
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-06 14:21:21 +0600

    header animated

--- components/Home/Header/Header.tsx
+++ components/Home/Header/Header.tsx
@@ -1,28 +1,35 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
 import HeaderItem from './HeaderItem/HeaderItem';
 
 const Header = () => {
+  const [anim, setanim] = useState(0)
   return (
     <div className=''>
       <Swiper
       slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
       loop={true}
+      onNavigationNext={e=> {setanim((p:number)=> p==0?1:0)}}
+      onNavigationPrev={e=> {setanim((p:number)=> p==0?1:0)}}
       autoplay={{
         delay:3000
       }}
-    
+      touchEventsTarget='wrapper'
+      navigation={true} 
+      allowTouchMove={false}
+      modules={[Navigation]}
+      
     >
-      <SwiperSlide>
-        <HeaderItem/>
+      <SwiperSlide key={0}>
+        <HeaderItem anim={anim==0}/>
       </SwiperSlide>
-      <SwiperSlide>
-        <HeaderItem/>
+      <SwiperSlide key={1}>
+        <HeaderItem anim={anim==1}/>
       </SwiperSlide>
     </Swiper>
     </div>
--- components/Home/Header/HeaderItem/HeaderItem.tsx
+++ components/Home/Header/HeaderItem/HeaderItem.tsx
@@ -1,17 +1,18 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 
-const HeaderItem = () => {
+const HeaderItem = ({anim}:{anim:boolean}) => {
   return (
     <div className='relative  h-[100vh] w-full top-0 left-0 right-0 bottom-0'>
         <Image src="/generator.jpg" height={500} width={600} alt='' className='absolute top-0 right-0 bottom-0 left-0 h-full object-cover w-full'/>
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70"></div>
         <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center px-5 md:px-10 z-30">
-            <p className='text-5xl uppercase font-bold text-white'>The Reliable Address <br /> <span className='text-primary'>for energy solutions</span></p>
+            <p className={`text-5xl uppercase font-bold transition-all ease-in-out duration-500 delay-300 ${anim?'ml-0 opacity-100':'-ml-10 opacity-0'} text-white`}>The Reliable Address <br /> <span className='text-primary'>for energy solutions</span></p>
         </div>
         
-        <div className="absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-tl from-white to-transparent">
-        <Image src="/solar.png" height={400} width={600} alt='' className='z-40 absolute bottom-0 right-5'/>
+        <div className={`absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-tl from-white to-transparent`}>
+        <Image src="/solar.png" height={400} width={600} alt='' className={`z-40 absolute transition-all ease-in-out delay-300 duration-500 bottom-0 ${anim?'right-5 opacity-100':'-right-5 opacity-0'}`}/>
         </div>
     </div>
   )
