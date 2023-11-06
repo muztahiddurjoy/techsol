28957b2bfd4c38174c4bf5f886af4b64b3686e49
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-06 14:24:09 +0600

    company profile done

--- components/CompanyProfile/CompanyBody/CompanyBody.tsx
+++ components/CompanyProfile/CompanyBody/CompanyBody.tsx
@@ -4,8 +4,11 @@ import React from 'react'
 
 const CompanyBody = () => {
   return (
-    <div className='grid grid-cols-5 py-10 md:px-20 px-5'>
-        <div className="col-span-3">
+    <div className='grid grid-cols-1 md:grid-cols-5 py-10 md:px-20 px-5'>
+        <div className="block md:hidden">
+            <Image src="/mission.jpg" height={500} width={300} className='h-full w-full md:pl-20' alt=''/>
+        </div>
+        <div className="col-span-3 mt-5 md:mt-0">
         <p className='font-bold text-gray-600'>Enmaksan Power is a leader in providing innovative and reliable solutions in the field of power generation and management. Our organization is recognized as an industry-leading player in power outages and energy needs. Our determination to provide uninterrupted energy to our customers and our commitment to sustainable energy are important features that distinguish us in the industry.</p>
         <p className='text-primary my-7 font-bold text-3xl'>Vision and Mission</p>
         <p className='text-gray-600'>At Enmaksan Power, we believe in shaping the future of the energy sector. Our vision is to increase energy efficiency worldwide, promote environmental sustainability and provide best practices to meet energy needs without interruption. Our mission is to design innovative energy solutions, deliver high quality products and provide our customers with a reliable partnership in energy.</p>
@@ -24,7 +27,7 @@ const CompanyBody = () => {
             </Link>
         </div>
         </div>
-        <div className="col-span-2">
+        <div className="md:col-span-2 hidden md:block">
             <Image src="/mission.jpg" height={500} width={300} className='h-full w-full md:pl-20' alt=''/>
         </div>
     </div>
