0bc5f2851cfc1dc086a164acfc532ac4d72f6826
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-23 15:32:59 +0600

    home header done

--- components/Home/Header/HeaderItem/HeaderItem.tsx
+++ components/Home/Header/HeaderItem/HeaderItem.tsx
@@ -4,15 +4,15 @@ import React from 'react'
 
 const HeaderItem = ({anim}:{anim:boolean}) => {
   return (
-    <div className='relative  h-[100vh] 2xl:h-[85vh] w-full top-0 left-0 right-0 bottom-0'>
-        <Image src="/generator.jpg" height={500} width={600} alt='' className='absolute top-0 right-0 bottom-0 left-0 h-full object-cover w-full'/>
+    <div className='relative h-[70vh] xl:h-[100vh] 2xl:h-[85vh] w-full top-0 left-0 right-0 bottom-0'>
+        <Image src="/generator.jpg" height={600} width={1000} alt='' className='absolute top-0 right-0 bottom-0 left-0 h-full object-cover w-full'/>
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70"></div>
         <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center px-5 md:px-10 z-30">
-            <p className={`text-5xl xl:text-6xl uppercase font-bold transition-all ease-in-out duration-1500 ${anim?'ml-20 2xl:ml-64 opacity-100':'-ml-10 opacity-0'} text-white`}>The Reliable Address <br /> <span className='text-primary'>for energy solutions</span></p>
+            <p className={`text-4xl md:text-5xl 2xl:text-6xl uppercase font-bold transition-all ease-in-out duration-1500 ${anim?'ml-5 2xl:ml-64 opacity-100':'-ml-10 opacity-0'} text-white`}>Your Partner <br /> <span className='text-primary'>for Power Solutions</span></p>
         </div>
         
-        <div className={`absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-tl 2xl:bg-gradient-to-t from-white to-transparent`}>
-        <Image src="/solar.png" height={1000} width={1000} alt='' className={`z-40 absolute transition-all ease-in-out duration-1500 2xl:w-[800px] bottom-0 2xl:bottom-16 ${anim?'right-20 2xl:right-64 2xl:bottom-16 opacity-100':'-right-5 opacity-0'}`}/>
+        <div className={`absolute h-[33vh] left-0 bottom-0 right-0 bg-gradient-to-t 2xl:bg-gradient-to-t from-white to-transparent`}>
+        <Image src="/solar.png" height={1000} width={1000} alt='' className={`z-40 absolute transition-all ease-in-out duration-1500 2xl:w-[800px] xl:w-[600px] w-[350px] bottom-0 2xl:bottom-16 ${anim?'md:right-20 right-0 2xl:right-64 2xl:bottom-16 opacity-100':'-right-5 opacity-0'}`}/>
         </div>
     </div>
   )
--- components/Home/InfoSecond/InfoSecond.tsx
+++ components/Home/InfoSecond/InfoSecond.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const InfoSecond = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-12 md:px-20 px-3 md:py-20 py-10 2xl:px-[364px]'>
+    <div className='grid grid-cols-1 md:grid-cols-12 md:px-20 px-3 md:py-20 py-10 2xl:px-[364px] mt-20'>
         <div className="col-span-7 md:pr-5 2xl:pr-10">
-            <p className='text-primary font-bold text-xl md:text-3xl lg:text-4xl'>DIFFERENT ENERGY SOLUTIONS</p>
-            <p className='text-slate-600 font-bold text-xl md:text-3xl lg:text-4xl'>DIFFERENT ENGINE OPTIONS</p>
+            <p className='text-primary font-bold text-3xl lg:text-4xl'>DIFFERENT ENERGY SOLUTIONS</p>
+            <p className='text-slate-600 font-bold text-3xl lg:text-4xl'>DIFFERENT ENGINE OPTIONS</p>
             <p className="mt-5 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum laboriosam nemo voluptate dolor deserunt voluptates earum eligendi corporis rerum maiores sed deleniti, sequi est distinctio culpa necessitatibus sit aspernatur et.</p>
             <div className="mt-3">
                 <BrandCarousel/>
