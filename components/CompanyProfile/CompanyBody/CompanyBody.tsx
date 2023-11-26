e1ffc1243b5460a2afdafd17aa414dec585f6aa3
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2023-11-27 00:56:52 +0600

    maybe done

--- components/CompanyProfile/CompanyBody/CompanyBody.tsx
+++ components/CompanyProfile/CompanyBody/CompanyBody.tsx
@@ -12,19 +12,11 @@ const CompanyBody = () => {
         <p className='font-bold text-gray-600'>Enmaksan Power is a leader in providing innovative and reliable solutions in the field of power generation and management. Our organization is recognized as an industry-leading player in power outages and energy needs. Our determination to provide uninterrupted energy to our customers and our commitment to sustainable energy are important features that distinguish us in the industry.</p>
         <p className='text-primary my-7 font-bold text-3xl'>Vision and Mission</p>
         <p className='text-gray-600'>At Enmaksan Power, we believe in shaping the future of the energy sector. Our vision is to increase energy efficiency worldwide, promote environmental sustainability and provide best practices to meet energy needs without interruption. Our mission is to design innovative energy solutions, deliver high quality products and provide our customers with a reliable partnership in energy.</p>
-        <div className="grid grid-cols-2 md:grid-cols-4 mt-5">
-            <Link href="#">
-                <Image height={70} width={200} src="/9001.png" className='p-3' alt=''/>
+        <div className="grid grid-cols-1 md:grid-cols-2 mt-5">
+            <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <Image height={70} width={200} src="/award.png" className='p-3' alt=''/>
             </Link>
-            <Link href="#" className='ml-3'>
-                <Image height={70} width={200} src="/9001.png" alt='' className='p-3'/>
-            </Link>
-            <Link href="#" className='ml-3'>
-                <Image height={70} width={200} src="/9001.png" className='p-3' alt=''/>
-            </Link>
-            <Link href="#" className='ml-3'>
-                <Image height={70} width={200} src="/9001.png" className='p-3' alt=''/>
-            </Link>
+           
         </div>
         </div>
         <div className="md:col-span-2 hidden md:block">
--- components/CompanyProfile/Header/Header.tsx
+++ components/CompanyProfile/Header/Header.tsx
@@ -15,12 +15,12 @@ const Header = () => {
   return (
     <div className="h-[90vh] 2xl:h-[65vh] w-full relative bg-[url('/generator.jpg')] bg-cover" style={{backgroundPositionY:scroll*0.5}}>
         <div className="bg-black/80 absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center px-5 md:px-20">
-            <p className='text-white text-center uppercase font-bold text-4xl lg:text-6xl'>COMNPANY PROFILE</p>
+            <p className='text-white text-center uppercase font-bold text-4xl xl:text-5xl 2xl:text-6xl'>COMNPANY PROFILE</p>
         </div>
         <div className="rotate-180 bottom-0 absolute left-0 right-0 -mb-0.5">
             <Triangle/>
         </div>
-        <Image src="/maksanOne.png" height={1000} width={1000} alt='' className='bottom-0 right-0 2xl:right-20 2xl:bottom-10 absolute w-full md:w-auto px-4 md:pr-10 2xl:pr-[246px] 2xl:w-[550px] xl:w-[500px] md:w-[400px] w-[250px]'/>
+        <Image src="/maksanOne.png" height={1000} width={1000} alt='' className='bottom-0 right-0 2xl:right-20 2xl:bottom-10 absolute px-4 md:pr-10 2xl:pr-[246px] 2xl:w-[550px] xl:w-[400px] md:w-[300px] w-[250px]'/>
     </div>
   )
 }
